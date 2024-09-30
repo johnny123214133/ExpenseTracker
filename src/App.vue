da87f1b693e183dbc014eed14a919c9e37ce2ef0
johnny123214133 <zach.j.richar@gmail.com>
2024-09-29 23:22:49 -0600

    added dynamic props to the other components to display data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
 	<Header />
 	<div class="container">
-		<Balance />
-		<IncomeExpenseSummary />
-		<TransactionList />
+		<Balance :total="total" /><!-- :total="total" -->
+		<IncomeExpenseSummary :income="income" :expenses="expenses" />
+		<TransactionList :transactions="transactions" />
 		<AddTransaction />
 
 	</div>
@@ -11,9 +11,53 @@
 </template>
 
 <script setup>
-	import Header from './components/Header.vue'
-	import Balance from './components/Balance.vue'
-	import IncomeExpenseSummary from './components/IncomeExpenseSummary.vue'
-	import TransactionList from './components/TransactionList.vue'
-	import AddTransaction from './components/AddTransaction.vue'
+import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpenseSummary from './components/IncomeExpenseSummary.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+
+import { ref, computed } from 'vue'
+
+const transactions = ref([
+	{id: 1, note: 'Flowers', amount: -19.99 },
+	{id: 2, note: 'Paycheck', amount: 1032.83 },
+	{id: 3, note: 'Book', amount: -27.99 },
+	{id: 4, note: 'Cough drops', amount: -7.99 },
+])
+
+// get total
+const total = computed(() => {
+	return transactions.value
+		.reduce((acc, transaction) => {
+			return acc + transaction.amount
+		}, 0)
+		.toFixed(2)
+})
+
+// get income
+const income = computed(() => {
+	return transactions.value
+		.filter((transaction) => { 
+			return transaction.amount >= 0 
+		})
+		.reduce((acc, transaction) => {
+			return acc + transaction.amount
+		}, 0)
+		.toFixed(2)
+})
+
+// get expenses
+const expenses = computed(() => {
+	return transactions.value
+		.filter((transaction) => { 
+			return transaction.amount < 0 
+		})
+		.reduce((acc, transaction) => {
+			return acc + transaction.amount
+		}, 0)
+		.toFixed(2)
+})
+
+
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -7,7 +7,7 @@
 			vbind:key=transaction.id 
 			:class="transaction.amount < 0 ? 'minus' : 'plus'"
 		>
-			{{transaction.text}}
+			{{transaction.note}}
 			<span v-if="transaction.amount >=0">${{ transaction.amount }}</span>
 			<span v-else>-${{ Math.abs(transaction.amount) }}</span>
 
@@ -18,31 +18,12 @@
 </template>
 
 <script setup>
-	//Composition API
+import { defineProps } from 'vue'
 
-	const transactions = [
-		{id : 1, text : 'Flowers', amount : -19.99 },
-		{id : 2, text : 'Paycheck', amount : 1032.83 },
-		{id : 3, text : 'Book', amount : -27.99 },
-		{id : 4, text : 'Cough drops', amount : -7.99 },
-	]
-
-</script>
-<!-- <script>
-	//Composition API
-	export default {
-		setup() {
-			const transactions = [
-				{id : 1, text : 'Flowers', amount : -19.99 },
-				{id : 2, text : 'Paycheck', amount : 1032.83 },
-				{id : 3, text : 'Book', amount : -27.99 },
-				{id : 4, text : 'Cough drops', amount : -7.99 },
-			]
-
-			// objects and methods
-			return { 
-				transactions
-			}
-		}
+const props = defineProps({
+	transactions : {
+		type : Array,
+		required : true
 	}
-</script> -->
+})
+</script>
